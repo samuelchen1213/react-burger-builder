457cf3ae508082befddabca00af6953df74e5d0b
samuelchen1213 <samuelchen23@icloud.com>
2019-04-29 19:53:38 -0400

    😆Style for Layout.js

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Aux from '../../HOC/Aux'
+import classes from './Layout.css'
 
 
 // ! Replace div with proper components
 const layout = (props) => (
     <Aux>
         <div>Toolbar, Sidedrawer, Backdrop</div> 
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
