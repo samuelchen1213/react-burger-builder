8f3a0e6b79d1b1740cce13334f09477b218b64cf
samuelchen1213 <samuelchen23@icloud.com>
2019-05-02 17:43:13 -0400

    😆Initialized Toolbar.js and Toolbar.css

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Aux from '../../HOC/Aux'
-
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import styles from './Layout.css'
 
 const layout = (props) => (
     <Aux>
-        
-        <div>Toolbar, Sidedrawer, Backdrop</div> 
+        <Toolbar/>
         <main className={styles.Content}>
             {props.children}
         </main>
