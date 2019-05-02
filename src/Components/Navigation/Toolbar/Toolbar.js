741f4335c8cb3473eb3fb021d059937ae86056c3
samuelchen1213 <samuelchen23@icloud.com>
2019-05-02 17:51:27 -0400

    😆Initlialized and Implemented Logo.js and Logo.css

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../../../Components/Logo/Logo'
 import classes from './Toolbar.css'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>
             ...
         </nav>
